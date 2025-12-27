9f0ee1604bd894e5f26486910f349cf36189c3b3
Sindhu Patil <sindhupat@gmail.com>
2025-12-27 11:08:59 -0500

    Initial Commit

---
+++ src/Dashboard/GetTitle.java
@@ -0,0 +1,23 @@
+package Dashboard;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GetTitle {
+
+    public static void main(String[] args) {
+      
+    	ChromeDriver driver;
+    	String url = "https://www.google.com";
+    	
+    	WebDriverManager.chromedriver().setup(); 
+    	driver = new ChromeDriver();
+    	
+    	driver.get(url);
+    	driver.getTitle();
+    	System.out.println("Title of the page is:" + driver.getTitle());
+    	
+    	driver.close();
+    	driver.quit();
+    }
+}
