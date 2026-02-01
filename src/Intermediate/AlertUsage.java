dc2f785e203329d018dd5f585c9d2fa6d998fc96
Sindhu Patil <sindhupat@gmail.com>
2026-01-31 22:34:18 -0500

    Alert

--- src/Intermediate/AlertUsage.java
+++ src/Intermediate/AlertUsage.java
@@ -1,10 +1,15 @@
 package Intermediate;
 
+import java.time.Duration;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.reporters.jq.TimesPanel;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,36 +21,48 @@ public class AlertUsage {
 		WebDriver driver;
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
+		
 		driver.get("https://www.qaplayground.com/practice?utm_source=chatgpt.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts();
+		
+		
 		Thread.sleep(500);
 		driver.findElement(By.xpath("(//span[normalize-space()='Dialog'])[1]")).click();
 		Thread.sleep(500);
 		
+		// Simple Alert
 		WebElement element = driver.findElement(By.xpath("//button[@id='simple-alert']"));
 		element.click();
+	
+		Alert SimpleAlert = driver.switchTo().alert();
+		System.out.println(SimpleAlert.getText());
 		
-		Alert simpleAlert = driver.switchTo().alert();
-		System.out.println(simpleAlert.getText());
-		simpleAlert.accept();
-		
+		Thread.sleep(300);
+		SimpleAlert.accept();
+		 Thread.sleep(300);
+		 
 		driver.findElement(By.xpath("//button[@id='confirm-alert']")).click();
-		simpleAlert = driver.switchTo().alert();
-		System.out.println(simpleAlert.getText());
-		simpleAlert.accept();
-		simpleAlert.di
+		SimpleAlert = driver.switchTo().alert();
+		System.out.println(SimpleAlert.getText());
+		SimpleAlert.accept();
+	
+		//Prompt Alert
+		/* driver.findElement(By.id("prompt-alert")).click();
+		 SimpleAlert = driver.switchTo().alert();
+		 Thread.sleep(400);
+		 SimpleAlert.sendKeys("test");
+		 SimpleAlert.accept();
+		 */
+		 //Toast Alerthttps://www.qaplayground.com/practice/alert
 		
-		driver.findElement(By.xpath(" //button[@id='prompt-alert']")).click();
-		simpleAlert = driver.switchTo().alert();
-		System.out.println(simpleAlert.getText());
-		simpleAlert.accept();
+		 WebElement toastMessage = driver.findElement(By.id("toast-alert"));
+		toastMessage.click();
+		 String message = toastMessage.getAttribute(null)
+		 System.out.println("Alert Toast message " + message);
 		
-		
-		
-		driver.close();
-		driver.quit();
-		
+		 driver.close();
+		 driver.quit();
 
 	}
 
